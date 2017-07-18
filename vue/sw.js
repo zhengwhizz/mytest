ab5fecb023bd6fdb070be84da2db9fd45955fa05
zj <zhengjun.whizz@gmai.com>
2017-07-18 19:47:57 +0800

    'init'

---
+++ vue/sw.js
@@ -0,0 +1,2 @@
+'use strict';
+importScripts('sw-toolbox.js'); toolbox.precache(["ani.html","ani.js"]); toolbox.router.get('/images/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
